0f323b93df53e6c18f12576af60bbaeb95bd65a5
nishankjain <nishankjain@users.noreply.github.com>
2019-03-04 14:38:46 -0500

    Implemented warnings.
    When status is changed to 'Ready', a warning is shown if any one of snapshot, hint, video or example is not present.

--- msadmin/static/js/qa/main.js
+++ msadmin/static/js/qa/main.js
@@ -6,7 +6,36 @@ function checkSnapshotValidity () {
 
 $('#saveButton').on('click', function() {
     var status = $('#my-pulldown3').val();
+    var warningString = "";
     if (status === 'ready' && $(this).data('snapshot') == 0) {
+        if ($(this).data('snapshot') == 0) {
+            warningString += "snapshot";
+        }
+        if ($(this).data('hint') == "True") {
+            if (warningString.length) {
+                warningString += " or hint";
+            }
+            else {
+                warningString += "hint";
+            }
+        }
+        if ($(this).data('video') == 0) {
+            if (warningString.length) {
+                warningString += " or video";
+            }
+            else {
+                warningString += "video";
+            }
+        }
+        if ($(this).data('example') == 0) {
+            if (warningString.length) {
+                warningString += " or example";
+            }
+            else {
+                warningString += "example";
+            }
+        }
+        $('#warning-text').text("You are saving the question without any " + warningString + ". Are you sure you want to continue?");
         $('#warningDialog').modal('show');
     }
     else {
