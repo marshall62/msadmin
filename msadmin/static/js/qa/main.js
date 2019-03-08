2461c1152dedaec9154022362f3e6b8f2fe8cd0a
nishankjain <nishankjain@users.noreply.github.com>
2019-03-08 01:26:50 -0500

    Warnings for name, nickname, description.

--- msadmin/static/js/qa/main.js
+++ msadmin/static/js/qa/main.js
@@ -7,38 +7,44 @@ function checkSnapshotValidity () {
 $('#saveButton').on('click', function() {
     var status = $('#my-pulldown3').val();
     var warningString = "";
-    if (status === 'ready' && $(this).data('snapshot') == 0) {
-        if ($(this).data('snapshot') == 0) {
-            warningString += "snapshot";
+    var formElement = document.getElementById('editProblem');
+    if (formElement.checkValidity()) {
+        if (status === 'ready') {
+            if ($(this).data('snapshot') == 0) {
+                warningString += "snapshot";
+            }
+            if ($(this).data('hint') == "True") {
+                if (warningString.length) {
+                    warningString += " or hint";
+                }
+                else {
+                    warningString += "hint";
+                }
+            }
+            if ($(this).data('video') == 0) {
+                if (warningString.length) {
+                    warningString += " or video";
+                }
+                else {
+                    warningString += "video";
+                }
+            }
+            if ($(this).data('example') == 0) {
+                if (warningString.length) {
+                    warningString += " or example";
+                }
+                else {
+                    warningString += "example";
+                }
+            }
+            if (warningString) {
+                event.preventDefault();
+                $('#warning-text').text("You are saving the question without any " + warningString + ". Are you sure you want to continue?");
+                $('#warningDialog').modal('show');
+            }
         }
-        if ($(this).data('hint') == "True") {
-            if (warningString.length) {
-                warningString += " or hint";
-            }
-            else {
-                warningString += "hint";
-            }
-        }
-        if ($(this).data('video') == 0) {
-            if (warningString.length) {
-                warningString += " or video";
-            }
-            else {
-                warningString += "video";
-            }
-        }
-        if ($(this).data('example') == 0) {
-            if (warningString.length) {
-                warningString += " or example";
-            }
-            else {
-                warningString += "example";
-            }
-        }
-        $('#warning-text').text("You are saving the question without any " + warningString + ". Are you sure you want to continue?");
-        $('#warningDialog').modal('show');
-    }
-    else {
-        $('#editProblem').submit();
+        // else {
+        //     $('#editProblem').submit();
+        // }
     }
 });
